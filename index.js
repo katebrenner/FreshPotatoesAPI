399a9629e40c23f88c489c245edbda2d962f0bf5
kate brenner <kate.e.brenner@gmail.com>
2018-02-09 23:06:47 -0500

    created sequelize query

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const sqlite = require('sqlite'),
       express = require('express'),
       app = express();
 
+      const sequelize = new Sequelize(
+        'database', 'username', 'password', {
+        host: 'localhost',
+        dialect: 'sqlite',
+
+        pool: {
+          max: 5,
+          min: 0,
+          acquire: 30000,
+          idle: 10000
+        },
+
+        // SQLite only
+        storage: './db/database.db'
+      });
+
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
 // START SERVER
@@ -16,12 +32,17 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body);
-    res.send(body) // Print the HTML for the Google homepage.
+  sequelize.query("SELECT * FROM `films`", { type: sequelize.QueryTypes.SELECT})
+  .then(films => {
+    res.send(films)
+    // We don't need spread here, since only the results will be returned for select queries
   })
+  // request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
+  //   // console.log('error:', error); // Print the error if one occurred
+  //   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //   console.log('body:', body);
+  //   res.send(body) // Print the HTML for the Google homepage.
+  // })
   // res.status(500).send('Not Implemented');
 
 }
