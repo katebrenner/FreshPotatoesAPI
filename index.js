41994d49865e4f21077a52bc44906e21babd90de
kate brenner <kate.e.brenner@gmail.com>
2018-02-09 19:55:24 -0500

    got a working api call

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body);
+    res.send(body) // Print the HTML for the Google homepage.
+  })
+  // res.status(500).send('Not Implemented');
+
 }
 
 module.exports = app;
