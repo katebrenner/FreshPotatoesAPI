026089000024ddd67db8f7f9ef45b7f29bac8abf
kate brenner <kate.e.brenner@gmail.com>
2018-02-11 16:21:25 -0500

    6 tests passing- api call returning data, but not all of it.  Must incorporate reviews and change release date logic

--- index.js
+++ index.js
@@ -32,18 +32,28 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  sequelize.query("SELECT * FROM `films`", { type: sequelize.QueryTypes.SELECT})
+    sequelize.query(`SELECT * FROM films WHERE genre_id = (
+      SELECT genre_id FROM films WHERE id = ${req.params.id})
+      AND release_date > (
+      SELECT release_date FROM films WHERE id = ${req.params.id})
+      ORDER BY id LIMIT 3`  ,
+
+      { type: sequelize.QueryTypes.SELECT})
+
   .then(films => {
-    res.send(films)
-    // We don't need spread here, since only the results will be returned for select queries
+    console.log('this is console.log for req. params: ', req.params)
+    console.log('this is console.log for genre_id: ', films[0].genre_id)
+    console.log('films dot release date: ', parseInt(films[0].release_date) + 15)
+    request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log(`this is how many reviews movie ${req.params.id} has` , JSON.parse(body)[0].reviews.length);
+      console.log('this is console.log films: ', films)
+      res.send({recommendations: films, meta: films.length})
+
+    })
+
   })
-  // request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
-  //   // console.log('error:', error); // Print the error if one occurred
-  //   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //   console.log('body:', body);
-  //   res.send(body) // Print the HTML for the Google homepage.
-  // })
-  // res.status(500).send('Not Implemented');
 
 }
 
